abbd2c915a124a6376899fbef98c4ce8296c0d44
SergeyUlikhanyan <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-23 02:32:40 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+input=$1
+res=0
+if [[ ${input:((${#input}-1)):1} = "C" ]]; then
+    ((res=${input:0:((${#input}-1))}+273))
+    echo $res"K"
+elif [[ ${input:((${#input}-1)):1} = "K" ]]; then
+    ((res=${input:0:((${#input}-1))}-273))
+     echo $res"C"
+fi
+
 # Place your code here
