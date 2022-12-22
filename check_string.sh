29dddb6dd4fa78dee1475057ea81d6bb10e7ca1b
SergeyUlikhanyan <84579276+SergeyUlikhanyan@users.noreply.github.com>
2022-12-23 02:52:16 +0400

    Update check_string.sh

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-# Place your code here
+input=$1
+
+letterCount=0
+numberCount=0
+symbolCount=0
+
+letter="[a-zA-Z]"
+number="[0-9]"
+symbols="+_!@#{}*-=~?/\|$%^&()"
+
+
+for (( i=0; i<${#input}; i++ ))
+do
+    count=${input:$i:1}
+    if [[ $count =~ $letter ]]
+    then
+        letterCount=$(($letterCount+1))
+    fi
+done
+
+for (( i=0; i<${#input}; i++ ))
+do
+    count=${input:$i:1}
+    if [[ $count =~ $number ]]
+    then
+        numberCount=$(($numberCount+1))
+    fi
+done
+
+for (( i=0; i<${#input}; i++ ))
+do
+    count=${input:$i:1}
+    if [[ $symbols == *"$count"* ]]
+    then
+        symbolCount=$(($symbolCount+1))
+    fi
+done
+
+echo "number: $numberCount Symbols: $symbolCount Letters: $letterCount"
